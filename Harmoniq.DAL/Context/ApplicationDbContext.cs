5413dfdb6dc3a9edde56b2b9b988bb6508244727
Ângelo Vidor <angelovidor5@gmail.com>
2024-12-11 18:39:32 -0300

    dbConnection

---
+++ Harmoniq.DAL/Context/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Harmoniq.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Harmoniq.DAL.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
+        {
+            
+        }
+        public DbSet<UserEntity> Users { get; set; }
+    }
+}
