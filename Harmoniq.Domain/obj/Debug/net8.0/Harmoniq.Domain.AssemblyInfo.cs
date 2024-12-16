e140426a2938255b66a625d02b9dad388efe8320
Ângelo Vidor <angelovidor5@gmail.com>
2024-12-16 13:36:34 -0300

    Committing changes in obj files

--- Harmoniq.Domain/obj/Debug/net8.0/Harmoniq.Domain.AssemblyInfo.cs
+++ Harmoniq.Domain/obj/Debug/net8.0/Harmoniq.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Harmoniq.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41459ae9f4de92d1d70bb32409f906df32f3d215")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ac839c5a9ed416fbf8adab0bc7e50b3b4ca38ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Harmoniq.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Harmoniq.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
